545485c7a34c2539f2da979ea14aad0f00eddd37
Johnexzy <obajohn75@gmail.com>
2020-04-25 16:32:48 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <nav>
+    <router-link to="/">HOME</router-link>
+    <router-link to="/about">ABOUT</router-link>  
+    </nav>
+    
+    <router-view/>
+    <!-- <HelloWorld/> -->
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components:{
+    HelloWorld
+  }
+}
+</script>
+<style>
+
+body{
+  background-color: #eeeeee;
+  display: grid;
+  grid-template-rows: auto;
+  justify-items: center;
+  align-items: center
+}
+body html{
+  width:100%;
+  height: 100%;
+}
+#app{
+  min-width: 300px;
+  margin: 0 auto;
+  font-family: monospace
+}
+nav{
+  padding: 20px 20px 20px 0px
+}
+nav a{
+  padding: 10px;
+  text-decoration: none;
+  background: #fff;
+  border-radius: 3px;
+  color: rgb(0, 110, 255);
+  font-weight: bold;
+  margin-right: 15px
+}
+</style>
---
+++ src/components/About.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="hello">   
+          <!-- <router-link to="/">ABOUT</router-link> -->
+          <img src="../assets/logo.png">
+    <h1>{{ mmg }}</h1>
+    <h2>Essential Links</h2>
+    <ul>
+      
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+        >
+          awesome-vue
+        </a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data () {
+    return {
+
+      mmg: 'Welcome '+this.$route.params.name
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
